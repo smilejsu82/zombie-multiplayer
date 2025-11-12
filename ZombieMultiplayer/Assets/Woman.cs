aa39b12b6e4dfaa9456ab122370e34bc1a9f9e3e
smilejsu82 <50188795+smilejsu82@users.noreply.github.com>
2025-11-12 16:07:15 +0900

    이동 및 애니메이션 적용
    이동 및 애니메이션 적용

--- ZombieMultiplayer/Assets/Woman.cs
+++ ZombieMultiplayer/Assets/Woman.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 
 public class Woman : MonoBehaviourPun
 {
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!photonView.IsMine)
+            return;
+            
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
         var dir = new Vector3(h, 0, v);
-        var q = Quaternion.LookRotation(dir);
-        transform.rotation = q;
-        transform.Translate(Vector3.forward * 1f * Time.deltaTime);
+        if (dir != Vector3.zero)
+        {
+            var q = Quaternion.LookRotation(dir);
+            transform.rotation = q;
+            transform.Translate(Vector3.forward * 2.1f * Time.deltaTime);
+            animator.SetInteger("State", 1);
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+        }
     }
 }
