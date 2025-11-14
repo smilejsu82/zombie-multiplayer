01ddbbc9eeb698b68a76655b1b7ed6f309fcb22d
smilejsu82 <50188795+smilejsu82@users.noreply.github.com>
2025-11-14 12:54:12 +0900

    준비버튼, 시작버튼 업데이트
    준비버튼, 시작버튼 업데이트

--- ZombieMultiplayer/Assets/RoomMain.cs
+++ ZombieMultiplayer/Assets/RoomMain.cs
@@ -35,6 +35,8 @@ public class RoomMain : MonoBehaviour
     private void OnPlayerLeftRoomEvent(short eventType)
     {
         uiPlayerList.UpdateUI(PhotonNetwork.CurrentRoom.Players.Values.ToList());
+        
+        UpdateReadyAndStartButton();
     }
 
     private void OnJoinedRoomEvent(short eventType)
